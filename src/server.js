d91d4f9b10097cdb41402a0967a00010ba0aa641
ComeONDuDE18 <vibilous6@gmail.com>
2025-09-14 00:23:30 +0200

    stepTwowithBug

--- src/server.js
+++ src/server.js
@@ -19,16 +19,23 @@ app.use(
     })
 );
 
-app.use( "*" , ( req, res, next ) => {
-    res.status(404).json( { message: 'Not found'} );
+app.use('*', (req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+app.use( ( err, req, res, next ) => {
+    res.status(500).json(
+        { message: "Something went wrong on the server side",
+            error: err.message }
+        
+    );
 } );
 
+
+
 app.listen(PORT, () => {
-    console.log("Server is running on port {PORT}");
+  console.log(`Server is running on port ${PORT}`);
 });
 }
-
-
-
-
-
