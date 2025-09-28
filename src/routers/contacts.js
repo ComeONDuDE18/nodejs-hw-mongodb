e74e5ef3e94c6fcf7c3f663247d8bd8a807ff234
ComeONDuDE18 <vibilous6@gmail.com>
2025-09-28 19:03:13 +0200

    createContact

---
+++ src/routers/contacts.js
@@ -0,0 +1,13 @@
+import {Router} from "express";
+import { getAllContactsController, getContactByIdController, createContactController } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const contactsRouter = Router();
+
+contactsRouter.get("/contacts",ctrlWrapper(getAllContactsController) );
+contactsRouter.get("/contacts/:contactId", ctrlWrapper(getContactByIdController));
+contactsRouter.post("/contacts", ctrlWrapper(createContactController));
+   
+
+export default contactsRouter;
+
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,10 @@ export const getContactById = async (contactId) => {
     const contact = await ContactCollection.findById(contactId);
     return contact;
 }   ;
+
+export const createContact = async (payload) => {
+    const newContact = await ContactCollection.create(payload);
+    return newContact;
+};
+
+
