44058cc1568ca7b7d790d2a1c074f63c237be16b
ComeONDuDE18 <vibilous6@gmail.com>
2025-10-14 23:58:16 +0200

    fixNew

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,15 +2,15 @@ import {Router} from "express";
 import { getAllContactsController, getContactByIdController, createContactController, putchContactController, deleteContactController }  from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import { validateBody } from "../middlewares/validateBody.js";
-import { contactSchema } from "../validation/contacts.js";
+import { createContactSchema, updateContactSchema } from "../validation/contacts.js";
 import { isValidId } from "../middlewares/isValid.js";
 
 const contactsRouter = Router();
 
 contactsRouter.get("/contacts",ctrlWrapper(getAllContactsController) );
 contactsRouter.get("/contacts/:contactId", isValidId, ctrlWrapper(getContactByIdController));
-contactsRouter.post("/contacts",validateBody(contactSchema), ctrlWrapper(createContactController));
-contactsRouter.patch("/contacts/:contactId", validateBody(contactSchema), isValidId, ctrlWrapper(putchContactController));
+contactsRouter.post("/contacts",validateBody(createContactSchema), ctrlWrapper(createContactController));
+contactsRouter.patch("/contacts/:contactId", validateBody(updateContactSchema), isValidId, ctrlWrapper(putchContactController));
 contactsRouter.delete("/contacts/:contactId", isValidId, ctrlWrapper(deleteContactController));
 
 
