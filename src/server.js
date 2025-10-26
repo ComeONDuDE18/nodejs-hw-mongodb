e644e28c113c33cbaee186a9551800c88acfa60d
ComeONDuDE18 <vibilous6@gmail.com>
2025-10-26 23:36:01 +0100

    EndOf7HomeWork

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import {errorHandler} from './middlewares/errorHandler.js';
 import {notFoundHandler} from './middlewares/notFoundHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 
 
@@ -20,6 +21,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use('/uploads', express.static(UPLOAD_DIR));
+app.use('/api-docs', swaggerDocs());
+
  app.use('/uploads', express.static(UPLOAD_DIR));
 
 app.use(
